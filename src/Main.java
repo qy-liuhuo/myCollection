0127caba5b4578abbac3f1e4cdefa44ae93686c6
mahaotian's debian <maht2001@163.com>
2024-07-23 16:28:04 +0800

    add:PriorityQueue

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,24 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Queue<Integer> q = new PriorityQueue<>();
-        ArrayList
+        long start = System.currentTimeMillis();
+        int t;
+        for (int j = 0; j < Integer.MAX_VALUE; j++){
+            for(int i = 0;i<Integer.MAX_VALUE;i++){
+                t = i / 2;
+            }
+        }
+
+        long mid = System.currentTimeMillis();
+        for (int j = 0; j < Integer.MAX_VALUE; j++){
+            for(int i = 0;i<Integer.MAX_VALUE;i++){
+                t = i >> 1;
+            }
+        }
+        long end = System.currentTimeMillis();
+        System.out.println(mid-start);
+        System.out.println(end-start);
+
+
     }
 }
--- src/indi/mahaotian/mycollection/queue/PriorityQueue.java
+++ src/indi/mahaotian/mycollection/queue/PriorityQueue.java
@@ -10,6 +10,8 @@ public class PriorityQueue<E> extends AbstractQueue<E> implements Serializable {
 
     private int size = 0;
 
+    private static final int MAX_ARRAY_SIZE = Integer.MAX_VALUE - 8;
+
     private static final int DEFAULT_INITIAL_CAPACITY = 11;
 
     private final Comparator<? super E> comparator;
@@ -17,7 +19,7 @@ public class PriorityQueue<E> extends AbstractQueue<E> implements Serializable {
 
     public PriorityQueue(int initCap, Comparator<? super E> comparator){
 
-        if(initCap < 1)
+        if(initCap < 1 || initCap > MAX_ARRAY_SIZE)
             throw new IllegalArgumentException();
         this.queue = new Object[initCap];
         this.comparator = comparator;
@@ -56,6 +58,7 @@ public class PriorityQueue<E> extends AbstractQueue<E> implements Serializable {
     @SuppressWarnings("unchecked")
     public PriorityQueue(PriorityQueue<? extends E> c) {
         this.comparator = (Comparator<? super E>) c.comparator();
+        initFromPriorityQueue(c);
     }
 
     @SuppressWarnings("unchecked")
@@ -65,16 +68,29 @@ public class PriorityQueue<E> extends AbstractQueue<E> implements Serializable {
     }
 
     private void initFromCollection(Collection<? extends E> c) {
+        initElementsFromCollection(c);
+        heapify();
     }
 
     private void initFromPriorityQueue(PriorityQueue<? extends E> pq) {
+        if(pq.getClass() == PriorityQueue.class){
+            this.queue = pq.toArray();
+            this.size = pq.size();
+        }else{
+            initFromCollection(pq);
+        }
     }
-
-    private Comparator<? super E> comparator() {
-        return this.comparator();
-    }
-
-    private void initElementsFromCollection(SortedSet<? extends E> ss) {
+    private void initElementsFromCollection(Collection<? extends E> c){
+        Object[] a = c.toArray();
+        if(c.getClass() != ArrayList.class)
+            a = Arrays.copyOf(a,a.length,Object[].class);
+        int len = a.length;
+        if (len == 1 || this.comparator != null) //??
+            for (Object o : a)
+                if (o == null)
+                    throw new NullPointerException();
+        this.queue = a;
+        this.size = a.length;
     }
 
     @Override
@@ -82,24 +98,143 @@ public class PriorityQueue<E> extends AbstractQueue<E> implements Serializable {
         return 0;
     }
 
+    private Comparator<? super E> comparator() {
+        return this.comparator;
+    }
+
     @Override
     public boolean offer(E e) {
-        return false;
+        if(e == null)
+            throw new NullPointerException();
+        int i = size;
+        if(i>=queue.length)
+            grow(i+1);
+        size++;
+        if(i==0)
+            queue[0] = e;
+        else
+            siftUp(i,e); //插入后向上调整
+        return true;
     }
 
+
+    @SuppressWarnings("unchecked")
     @Override
     public E poll() {
-        return null;
+        if(size == 0)
+            return null;
+        size--;
+        E result = (E) queue[0];
+        E last = (E) queue[size];
+        queue[size] = null;
+        if(size != 0)
+            siftDown(0,last);
+        return result;
     }
-
+    @SuppressWarnings("unchecked")
     @Override
     public E peek() {
-        return null;
+        if(size == 0)
+            return null;
+        return (E) queue[0];
     }
 
     @Override
     public Iterator<E> iterator() {
         return null;
+    }
+
+    private void grow(int targetCapacity){
+        int oldCapacity = queue.length;
+        int newCapacity = oldCapacity + ((oldCapacity<64)? (oldCapacity+2):(oldCapacity>>1));
+        newCapacity = Math.max(newCapacity,targetCapacity);
+        newCapacity = Math.min(newCapacity,MAX_ARRAY_SIZE);
+        queue = Arrays.copyOf(queue,newCapacity);
+    }
+
+    @SuppressWarnings("unchecked")
+    private void heapify() {
+        for(int i = (size >>> 1) - 1; i >= 0; i--)
+            siftDown(i, (E) this.queue[i]);
+    }
+
+    private void siftUp(int i, E e) {
+        if (comparator != null)
+            siftUpWithComparator(k, x);
+        else
+            siftUpWithoutComparator(k, x);
+    }
+
+    @SuppressWarnings("unchecked")
+    private void siftUpWithComparator(int k, E x){
+        while (k > 0) {
+            int parent = (k - 1) >>> 1;
+            Object e = queue[parent];
+            if (comparator.compare(x, (E) e) >= 0)
+                break;
+            queue[k] = e;
+            k = parent;
+        }
+        queue[k] = x;
+    }
+
+    @SuppressWarnings("unchecked")
+    private void siftUpWithoutComparator(int k, E x){
+        Comparable<? super E> key = (Comparable<? super E>) x;
+        while (k > 0) {
+            int parent = (k - 1) >>> 1;
+            Object e = queue[parent];
+            if (key.compareTo((E) e) >= 0)
+                break;
+            queue[k] = e;
+            k = parent;
+        }
+        queue[k] = key;
+    }
+
+    private void siftDown(int k, E x) {
+        if(comparator != null)
+            siftDownWithComparator(k,x);
+        else
+            siftDownWithoutComparator(k,x);
+    }
+    @SuppressWarnings("unchecked")
+    private void siftDownWithoutComparator(int k, E x) {
+        Comparable<? super E> key = (Comparable<? super E>) x; // 获取comparable对象，来使用compareto方法，super说明E是下界，如果E没有实现comparable接口，会向上查找
+        int half = size>>>1;
+        while(k < half){
+            int child = (k<<1) + 1;
+            Object c = queue[child];
+            int right = child + 1;
+            //如果右节点小于左节点则更新child
+            if(right < size && ((Comparable<? super E>)c).compareTo((E) queue[right])>0){
+                c = queue[child = right];
+            }
+            if(key.compareTo((E) c) <=0)
+                break; //无需调整
+            queue[k] = c;
+            k = child;
+        }
+        queue[k] = x;
+    }
+
+    @SuppressWarnings("unchecked")
+    private void siftDownWithComparator(int k, E x) {
+        int half = size >>> 1;
+        while(k < half){
+            int child = (k<<1) + 1;
+            Object c = queue[child];
+            int right = child + 1;
+            //如果右节点小于左节点则更新child
+            if(right < size && comparator.compare((E) c,(E) queue[right]) > 0){
+                c = queue[child = right];
+            }
+            if(comparator.compare(x,(E) c) <= 0) //根节点小于子节点
+                break; //无需调整
+            queue[k] = c;
+            k = child;
+        }
+        queue[k] = x;
     }
 
 }
